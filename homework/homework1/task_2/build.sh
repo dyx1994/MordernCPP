c026c03cfee9abbb93247653a73d03ade495b82a
Yongxu Duan <duanyongxu1994@gmail.com>
2022-04-19 22:11:47 +0800

    Homework1 1st assignment

---
+++ homework/homework1/task_2/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo "Compile main program and arithmetic functions..."
+c++ -std=c++17 -c ./src/main.cpp -o ./build/main.o -I./include
+c++ -std=c++17 -c ./src/subtract.cpp -o ./build/subtract.o -I ./include
+c++ -std=c++17 -c ./src/sum.cpp -o ./build/sum.o -I ./include
+echo "Organize two functions into a library..."
+ar rcs ./results/lib/libipb_arithmetic.a ./build/sum.o ./build/subtract.o
+echo "LInk library with main program..."
+c++ -std=c++17 ./src/main.cpp -L ./results/lib/ -lipb_arithmetic -o ./results/bin/test_ipb_arithmetic -I ./include
+echo "Run the program..."
+cd results/bin
+./test_ipb_arithmetic
